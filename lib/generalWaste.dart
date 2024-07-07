865fa173c5a9bb7ab75158dedd4004660635c758
GINGERMAN <bosssnkc@hotmail.com>
2024-07-07 16:51:16 +0700

    minor update

--- lib/generalWaste.dart
+++ lib/generalWaste.dart
@@ -1,4 +1,4 @@
-import 'dart:typed_data';
+// import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:zerowastehero/database/database_helper.dart';
 import 'package:zerowastehero/database/trash_crud.dart';
