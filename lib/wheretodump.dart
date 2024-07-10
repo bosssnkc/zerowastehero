4ce1916320d93c8687113dbfce6db9e09965977d
GINGERMAN <bosssnkc@hotmail.com>
2024-07-10 20:59:28 +0700

    update Google Map
    update Google Map can get exactly location from now

--- lib/wheretodump.dart
+++ lib/wheretodump.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class recycleLocation extends StatefulWidget {
@@ -12,6 +13,47 @@ class _recycleLocationState extends State<recycleLocation> {
   late GoogleMapController mapController;
 
   final LatLng _center = const LatLng(13.736717, 100.523186);
+  Position? userLocation;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _getLocation();
+  // }
+
+  Future<void> _getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Handle location services disabled
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Handle permission denied
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Handle permission denied permanently
+      return;
+    }
+
+    final position = await Geolocator.getCurrentPosition();
+    setState(() {
+      userLocation = position;
+    });
+
+    mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: LatLng(userLocation!.latitude, userLocation!.longitude),
+        zoom: 15.0)));
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -50,17 +92,25 @@ class _recycleLocationState extends State<recycleLocation> {
                       SizedBox(
                         width: 50,
                       ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'ตำแหน่งปัจจุบันของคุณ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 10),
-                          Text('{location now}')
-                        ],
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'ตำแหน่งปัจจุบันของคุณ',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            userLocation != null
+                                ? Text(
+                                    'Lat: ${userLocation!.latitude} Lon: ${userLocation!.longitude}')
+                                : Text('ไม่พบตำแหน่งปัจจุบัน'),
+                            SizedBox(height: 10),
+                            ElevatedButton(
+                                onPressed: _getLocation,
+                                child: Text('Get Location'))
+                          ],
+                        ),
                       ), //Column inside location_now
                     ],
                   ),
@@ -79,9 +129,23 @@ class _recycleLocationState extends State<recycleLocation> {
                   height: 300,
                   child: Center(
                       child: GoogleMap(
-                          initialCameraPosition: CameraPosition(
-                              target: _center,
-                              zoom: 11.0)) //Text('{location_recycleshop}'),
+                          onMapCreated: _onMapCreated,
+                          initialCameraPosition:
+                              CameraPosition(target: _center, zoom: 11.0),
+                          markers: userLocation != null
+                              ? {
+                                  Marker(
+                                    markerId: MarkerId('userLocation'),
+                                    position: LatLng(
+                                      userLocation!.latitude,
+                                      userLocation!.longitude,
+                                    ),
+                                    infoWindow: InfoWindow(
+                                      title: 'ตำนแห่งปัจจุบันของคุณ',
+                                    ),
+                                  )
+                                }
+                              : {}) //Text('{location_recycleshop}'),
                       ),
                 ),
               )
@@ -101,6 +165,53 @@ class hazardousLocation extends StatefulWidget {
 }
 
 class _hazardousLocationState extends State<hazardousLocation> {
+  late GoogleMapController mapController;
+  Position? userLocation;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _getLocation();
+  // }
+
+  Future<void> _getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Handle location services disabled
+      return;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Handle permission denied
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Handle permission denied permanently
+      return;
+    }
+
+    final position = await Geolocator.getCurrentPosition();
+    setState(() {
+      userLocation = position;
+    });
+
+    mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: LatLng(userLocation!.latitude, userLocation!.longitude),
+        zoom: 15.0)));
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -134,17 +245,25 @@ class _hazardousLocationState extends State<hazardousLocation> {
                       SizedBox(
                         width: 50,
                       ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'ตำแหน่งปัจจุบันของคุณ',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(height: 10),
-                          Text('{location now}')
-                        ],
+                      Expanded(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'ตำแหน่งปัจจุบันของคุณ',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            userLocation != null
+                                ? Text(
+                                    'Lat: ${userLocation!.latitude} Lon: ${userLocation!.longitude}')
+                                : Text('ไม่พบตำแหน่งปัจจุบัน'),
+                            SizedBox(height: 10),
+                            ElevatedButton(
+                                onPressed: _getLocation,
+                                child: Text('Get Location'))
+                          ],
+                        ),
                       ), //Column inside location_now
                     ],
                   ),
@@ -163,19 +282,42 @@ class _hazardousLocationState extends State<hazardousLocation> {
                   height: 300,
                   child: Center(
                     child: GoogleMap(
+                      onMapCreated: _onMapCreated,
                       markers: {
-                        const Marker(
-                            markerId: MarkerId('thisLocation'),
-                            icon: BitmapDescriptor.defaultMarker,
-                            position: LatLng(13.736717, 100.523186),
-                            infoWindow: InfoWindow(title: 'ร้านขายข้าว')),
+                        userLocation != null
+                            ? Marker(
+                                markerId: MarkerId('userLocation'),
+                                position: LatLng(
+                                  userLocation!.latitude,
+                                  userLocation!.longitude,
+                                ),
+                                infoWindow: InfoWindow(
+                                  title: 'ตำนแห่งปัจจุบันของคุณ',
+                                ),
+                              )
+                            : Marker(
+                                markerId: MarkerId('thisLocation'),
+                                icon: BitmapDescriptor.defaultMarker,
+                                position: LatLng(13.736717, 100.523186),
+                                infoWindow: InfoWindow(title: 'ร้านขายข้าว')),
                         const Marker(
                             markerId: MarkerId('test2'),
                             icon: BitmapDescriptor.defaultMarker,
                             position: LatLng(14.736717, 100.523186))
                       },
-                      initialCameraPosition: const CameraPosition(
-                          target: LatLng(13.736717, 100.523186), zoom: 11.0),
+                      initialCameraPosition: CameraPosition(
+                          target: userLocation != null
+                              ? LatLng(
+                                  userLocation!.latitude,
+                                  userLocation!.longitude,
+                                )
+                              : LatLng(13.736717, 100.523186),
+                          // target: LatLng(
+                          //   userLocation!.latitude,
+                          //   userLocation!.longitude,
+                          // ),
+                          // target: LatLng(13.736717, 100.523186),
+                          zoom: 11.0),
                     ),
                   ),
                 ),
